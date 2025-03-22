b4bf526b0aa10c3dd934770d31406a9a77bfc158
Lates Codrin-Gabriel <codrin.lates@stud.ubbcluj.ro>
2025-03-22 15:00:00 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -68,7 +68,11 @@ def check_for_changes():
             last_checked_time[group_nr] = time.strftime("%Y-%m-%d %H:%M:%S")
 
 
-@app.get("/api/py/orar/{grupa}")
+
+@app.get("/")
+async def get_home():
+    return "Schedule Monitor front page. Try /api/orar/group_number!"
+@app.get("/orar/{grupa}")
 async def get_timetable(grupa: int, background_tasks: BackgroundTasks):
     """Returns the timetable and triggers an update in the background."""
     if grupa not in last_data:
